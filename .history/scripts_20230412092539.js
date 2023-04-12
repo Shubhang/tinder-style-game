008e7568be4e0cf64e33a3c7d7098050fa66a5d9
ssharma616 <shubhang@gatech.edu>
2023-04-12 09:29:24 -0400

    cards move now

---
+++ .history/scripts_20230412092539.js
@@ -0,0 +1,35 @@
+const cardContainer = document.querySelector(".card-container");
+const card = document.querySelector(".card");
+
+const hammer = new Hammer(cardContainer);
+
+hammer.on("swipeleft swiperight", (event) => {
+  if (event.type === "swipeleft") {
+    card.style.transform = "translateX(-150%)";
+    navigateToSet1();
+  } else {
+    card.style.transform = "translateX(150%)";
+    navigateToSet2();
+  }
+  setTimeout(() => {
+    resetCard();
+  }, 1000);
+});
+
+function resetCard() {
+  card.style.transition = "none";
+  card.style.transform = "translateX(0)";
+  setTimeout(() => {
+    card.style.transition = "transform 0.5s";
+  }, 100);
+}
+
+function navigateToSet1() {
+  console.log("Navigating to set 1...");
+  // Add logic to load cards from set 1
+}
+
+function navigateToSet2() {
+  console.log("Navigating to set 2...");
+  // Add logic to load cards from set 2
+}
